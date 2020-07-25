4545dc9df0dc42e788c516a5a48b0b90519c1313
jonghyun <heiwais25@gmail.com>
2020-07-26 00:45:53 +0900

    Routing by search bar
    - Clear in the default bar
    - Search routes to new page

--- src/common/components/molecules/SearchBox/SearchBox.tsx
+++ src/common/components/molecules/SearchBox/SearchBox.tsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, KeyboardEvent } from "react";
 import { TextField, InputAdornment } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
 
-export default () => (
-  <TextField
-    variant="outlined"
-    fullWidth
-    InputProps={{
-      startAdornment: (
-        <InputAdornment position="start">
-          <SearchIcon />
-        </InputAdornment>
-      ),
-    }}
-  />
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      background: "white",
+    },
+  })
 );
+
+type IProps = {
+  term: string;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleEnter: () => void;
+};
+
+export default ({ term, handleChange, handleEnter }: IProps) => {
+  const classes = useStyles();
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.which === 13) {
+      handleEnter();
+    }
+  };
+
+  return (
+    <TextField
+      variant="outlined"
+      className={classes.root}
+      fullWidth
+      value={term}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+};
